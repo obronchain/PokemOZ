58b98615c67471106a3794b11817ce6e0dea8f0c
obronchain <olivier.bronchain@student.uclouvain.be>
2015-05-04 16:16:25 +0200

    newScriptTo start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #! 
-ozc -c PokemOZ.oz
-ozc -c MyFunctor.oz
+fileWarning=x"warning"
+
+if [ -e $fileWarning ]
+then
+	rm $fileWarning
+fi
+
+echo "please wait. Compiling the PokemOZ project"
+ozc -c PokemOZ.oz &> $fileWarning
+echo "first file compiled. PokemOZ.ozf created"
+ozc -c MyFunctor.oz &> $fileWarning
+echo "second file compiled. MyFunctor.ozf created"
 ozengine MyFunctor.ozf
+echo "to restart the game, excecute: ozengine MyFunctor.ozf"
